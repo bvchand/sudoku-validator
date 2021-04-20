f0ed10403fdf460fde8e7f84634982bb5678faf8
karthik kashyap <kashyapkarthik3@gmail.com>
2021-04-20 12:13:34 -0700

    Added sudoku single threaded validation

--- sudoku-solution-validator/src/sudoku-solution-validator.c
+++ sudoku-solution-validator/src/sudoku-solution-validator.c
@@ -23,6 +23,17 @@ typedef struct {
 	int (*matrix)[ROWS];
 }parameters;
 
+int checkForDuplicate(int nums[]){
+    int total = 0;
+    for(int i=0;i<9;i++){
+        total+=nums[i];
+    }
+    if(total != 45){
+        return 0;
+    } 
+    return 1;
+}
+
 
 int main(void) {
 	FILE *fp;
@@ -42,6 +53,56 @@ int main(void) {
 			printf("\n");
 		}
 	}
+
+    //Checking rows
+    int rows[9];
+    for(int col=0; col < COLUMNS; col++){
+        if(!checkForDuplicate(matrix[col])){
+            printf("invalid sudoku for col:\n");
+            for(int x=0;x<9;x++){
+                printf("%d ",matrix[col][x]);
+            }
+            exit(0);
+        }
+    }
+
+    //Checking columns
+    for(int x=0;x<9;x++){
+        for(int y = 0;y<9;y++){
+            rows[y]=matrix[y][x];
+        }
+        if(!checkForDuplicate(rows)){
+            printf("invalid sudoku loool\n");
+            exit(0);
+        }
+    }
+
+    //Checking for 3X3 grids
+    for(int subgridrow = 0;subgridrow<9;subgridrow+=3){
+        for(int subgridcol = 0;subgridcol<9;subgridcol+=3){
+            int rowsIndex =0;
+            for(int row=0;row<3;row++){
+                for(int col=0;col<3;col++){
+                    rows[rowsIndex++] = matrix[subgridrow + row][subgridcol + col];
+                    // printf("%d ",matrix[subgridrow + row][subgridcol + col]);
+                }
+                // printf("\n");
+            }
+            // for(int xx=0;xx<9;xx++){
+            //     printf("%d ",rows[xx]);
+            // }
+            // printf("\n\n");
+            if(!checkForDuplicate(rows)){
+                printf("invalid sudoku who let the dogs otu\n");
+                for(int x=0;x<9;x++){
+                    printf("%d ",rows[x]);
+                }
+                exit(0);
+            }
+        }
+    }
+
+
 	fclose(fp);
 
 	for(int i=0; i<ROWS; i++) {
