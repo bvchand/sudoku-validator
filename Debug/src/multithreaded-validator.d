f36387dcd7347a404c610394ca7165a9e62eafa1
bvchand <cbharathividhya@gmail.com>
2021-04-22 15:38:01 -0700

    removing files

--- Debug/src/multithreaded-validator.d
+++
@@ -1,7 +0,0 @@
-src/multithreaded-validator.o: ../src/multithreaded-validator.c \
-  ../src/../inc/multithreaded-validator.h \
-  ../src/../inc/../inc/sudoku-solution-validator.h
-
-../src/../inc/multithreaded-validator.h:
-
-../src/../inc/../inc/sudoku-solution-validator.h:
--- Debug/src/single-threaded-validator.d
+++
@@ -1,7 +0,0 @@
-src/single-threaded-validator.o: ../src/single-threaded-validator.c \
-  ../src/../inc/single-threaded-validator.h \
-  ../src/../inc/../inc/sudoku-solution-validator.h
-
-../src/../inc/single-threaded-validator.h:
-
-../src/../inc/../inc/sudoku-solution-validator.h:
